d61665704899921269cb0fef670db64dd00828a9
Clinton Page <clinton.page123@gmail.com>
2019-06-22 23:37:00 +0930

    working on counting

--- 2017/quals/2017-re-counting/solution/counter-new.py
+++ 2017/quals/2017-re-counting/solution/counter-new.py
@@ -22,14 +22,36 @@ class Counter():
 	def counter(self, key_buffer, line_num):
 		while line_num != self.TOTAL_LINES:
 			current_line = line_num << 4
-			print("current_line = " + str(hex(current_line)))
 			headerByte = self.toDWord(self.CODE_FILEPOINTER[current_line:current_line+4])
+			
+			print("(0x%x) - " % (current_line), end ="")
+			
+			#special cases
+			if current_line == 0x0:
+				print("key_buffer[0x8] = key_buffer[0x0]")
+				key_buffer[0x8:0x8 + 8] = key_buffer[0x0:0x0+8]
+				print("key_buffer[0x0] = 0x0")
+				key_buffer[0x0:0x0 + 8] = struct.pack("<q", 0)
+				line_num = 0x02
+				continue
+			if current_line == 0xd0:
+				print("nop - jump to 0x0e")
+				line_num = 0x0e
+				continue
+			#if current_line == 0x1e0:
+			#	value1 = self.toQWord(key_buffer[0x0:0x0+8])
+			#	if value1 != 0:
+			#		print("WARNING")
+			#	print("key_buffer[0x0] = key_buffer[0x8]")
+			#	continue
+			
 			if headerByte == 0:
 				#add 1 at key_address
 				key_address = self.CODE_FILEPOINTER[current_line+4] * 8
-				line_num = self.toDWord(self.CODE_FILEPOINTER[current_line+8:current_line+8+4])
 				_temp = self.toQWord(key_buffer[key_address:key_address+8]) + 1
 				self.packQWord(key_buffer, key_address, _temp)
+				line_num = self.toDWord(self.CODE_FILEPOINTER[current_line+8:current_line+8+4])
+				print("add 1, at = 0x%x, new_val = %s" % (key_address, _temp))
 			if headerByte == 1:
 				#subtract 1 at key_address if already zero then jump to different spot
 				key_address = self.CODE_FILEPOINTER[current_line+4] * 8
@@ -39,17 +61,21 @@ class Counter():
 					
 					line_num = self.toDWord(self.CODE_FILEPOINTER[current_line+8:current_line+8+4])
 					self.packQWord(key_buffer, key_address, Qword_value)
+					print("sub 1, at = 0x%x, new_val = %s" % (key_address, Qword_value))
 				else:
 					line_num = self.toDWord(self.CODE_FILEPOINTER[current_line+12:current_line+12+4])
+					print("zero , at = 0x%x, jumping" % (key_address))
 			if headerByte == 2:
 				#call counter from new_next_line copy result if firstDWord greater than zero
 				new_buffer = key_buffer[:]
 				new_next_line = self.toDWord(self.CODE_FILEPOINTER[current_line+8:current_line+8+4])
+				print("call , to = 0x%x" % (new_next_line))
 				self.counter(new_buffer, new_next_line)
 				firstQWord = self.toDWord(self.CODE_FILEPOINTER[current_line+4:current_line+4+4])
 				if firstQWord != 0:
 					key_buffer[0:firstQWord*8] = new_buffer[0:firstQWord*8]
 				line_num = self.toDWord(self.CODE_FILEPOINTER[current_line+12:current_line+12+4])
+		print("exiting counter, key = " + str(binascii.hexlify(key_buffer)))
 					
 					
 	
